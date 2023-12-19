968a24cad9f034cee174d47e2960bcd0498e9e0b
DevLung <immo.friedhoff@web.de>
2023-12-19 23:29:37 +0100

    fix resetting volume sliders after scene reload

--- Assets/Scripts/HighScoreResetLogicScript.cs
+++ Assets/Scripts/HighScoreResetLogicScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class HighScoreResetLogicScript : MonoBehaviour
