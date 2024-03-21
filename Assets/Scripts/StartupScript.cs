14248f7016274d9346ce91d347046dc43f2546c9
DevLung <immo.friedhoff@web.de>
2024-03-21 05:52:59 +0100

    finish implementing update checker backend

--- Assets/Scripts/StartupScript.cs
+++ Assets/Scripts/StartupScript.cs
@@ -11,9 +11,13 @@ public class StartupScript : MonoBehaviour
 {
     public Text text;
     const string latestVersionInfoUrl = "https://api.github.com/repos/DevLung/FLAPPY.ZIP/releases/latest";
+    int currentVersion;
+    int latestVersion;
 
     void Start()
     {
+        currentVersion = latestVersion = Int32.Parse(Application.version.Replace(".", string.Empty));
+
         CheckForUpdates();
         SceneManager.LoadSceneAsync("MainGame");
     }
@@ -27,6 +31,25 @@ public class StartupScript : MonoBehaviour
         while (!operation.isDone)
         {
             await Task.Yield();
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            // extract tag name/version number from response
+            string rawLatestVersion = System.Text.RegularExpressions.Regex.Match(
+                    request.downloadHandler.text,
+                    "(?<=\"tag_name\"\\s*:\\s*\")[^\"]*(?=\")"      // (?<="tag_name"\s*:\s*")[^"]*(?=")   finds  "tag_name":  and matches the following characters between the next two "
+                ).ToString();
+
+            try
+            {
+                latestVersion = Int32.Parse(rawLatestVersion.Replace(".", string.Empty));
+            } catch (FormatException)
+            {
+                return;
+            }
+
+            Debug.Log(currentVersion < latestVersion); // TODO implement new version available screen
         }
     }
 
